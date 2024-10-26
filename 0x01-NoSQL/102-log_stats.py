206d276f90b71a0b53528dbd77410d701e477977
Faithlicious <you@violetshiko@gmail.com>
2024-10-26 07:59:04 +0000

    0x01-NoSQL

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,39 +1,30 @@
 #!/usr/bin/env python3
-"""
-Provide some stats about Nginx logs stored in MongoDB
-Database: logs, Collection: nginx, Display same as example
-first line: x logs, x number of documents in this collection
-second line: Methods
-5 lines with method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-one line with method=GET, path=/status
-"""
+""" Defines a program that prints nginx stats in a mongo db """
+
 from pymongo import MongoClient
 
 
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-
-def log_stats(mongo_collection, option=None):
-    """
-    Prototype: def log_stats(mongo_collection, option=None):
-    Provide some stats about Nginx logs stored in MongoDB
-    """
-    items = {}
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
-    print("Methods:")
-    for method in METHODS:
-        log_stats(nginx_collection, method)
-    status_check = mongo_collection.count_documents({"path": "/status"})
-    print(f"{status_check} status check")
-
-
 if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    log_stats(nginx_collection)
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['logs']
+    collection = db['nginx']
+    all = [i for i in collection.find()]
+    ips = collection.aggregate([{"$group": {"_id": "$ip", "num": {"$sum": 1}}},
+                                {"$sort": {"num": -1}}])
+    ips = [i for i in ips][:10]  # get the top 10
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print(f"{len(all)} logs")
+    print("Methods:")
+
+    for i in methods:
+        print("\tmethod {}: {}"
+              .format(i, len([j for j in all if j["method"] == i])))
+
+    print("{} status check".format(len(list(collection.find({"method": "GET",
+                                            "path": "/status"})))))
+
+    print("IPs:")
+    for i in ips:
+        print('\t{}: {}'.format(i["_id"], i["num"]))
